6a1ae2a7b836724e632ff01af46376f5ef4cb51e
Mehr Kaur <mehrkaur23@gmail.com>
2020-08-14 18:22:09 -0500

    barebones webapp

--- webapp/app.py
+++ webapp/app.py
@@ -20,11 +20,14 @@ def hello():
 @app.route("/96th")
 def f96th():
     
-    #dateRequest = date(datetime.now())
-    #timeRequest = time(datetime.now())
-    #entries, crowdSize = get_crowds(dateRequest,timeRequest,'120')
-    #crowdRank = grade_crowds(crowdSize)
-    #result = str(crowdSize[0])
+    dateRequest = datetime.date(datetime.now())
+    timeRequest = datetime.time(datetime.now())
+    entries, crowdSize = get_crowds(dateRequest,timeRequest,'120')
+    crowdRank = grade_crowds(crowdSize)
+    northNum = str(crowdSize[0])
+    northRisk = str(crowdRank[0])
+    southNum = str(crowdSize[1])
+    southRisk = str(crowdRank[1])
     
     #print("There are " + str(crowdSize[0]) + " people on the Northbound platform.")
     #print("The Northbound platform is at risk level " + str(crowdRank[0]) + ".")
@@ -32,15 +35,19 @@ def f96th():
     #print("The Southbound platform is at risk level " + str(crowdRank[1]) + ".")
     
     #result = do_calculation(number1, number2)
-    #return '''
-        #<html>
-            #<body>
-                #<h1>Stay Subway Safe</h1>
-                #<p>There are {result} people on the Northbound platform.</p>
-            #</body>
-        #</html>
-    #'''.format(result=result)
-    return render_template("96th.html") 
+    return '''
+        <html>
+            <body>
+                <h1>Stay Subway Safe</h1>
+                <h2>96th Street, now</h2>
+                <p>There are {0} people on the Northbound platform.</p>
+                <p>The Northbound platform is at risk level {1}.</p>
+                <p>There are {2} people on the Southbound platform.</p>
+                <p>The Southbound platform is at risk level {3}.</p>
+            </body>
+        </html>
+    '''.format(northNum,northRisk,southNum,southRisk)
+    #return render_template("96th.html") 
     #<p>The result is {result}</p>
 #start the server
 if __name__ == "__main__":
